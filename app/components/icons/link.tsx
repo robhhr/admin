45d35480ceb9ffc4b31cf43bb7d3e4ac832fd831
robhhr <robertohhr@pm.me>
2025-05-12 00:50:16 -0700

    add project icons

---
+++ app/components/icons/link.tsx
@@ -0,0 +1,19 @@
+export const IconLinkExternal = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"
+    >
+      <path d="M7 7h10v10" />
+      <path d="M7 17 17 7" />
+    </svg>
+  )
+}
