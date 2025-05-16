7e94ce3a694c3a538be912bc3e08eb6cc2b4f8ce
robhhr <robertohhr@pm.me>
2025-05-15 23:38:39 -0700

    add archive opt

--- app/components/admin/project.tsx
+++ app/components/admin/project.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router'
+import {Link, useFetcher} from 'react-router'
 import type {Project as ProjectProps} from '~/models/projects.server'
 
 interface Props {
@@ -6,11 +6,12 @@ interface Props {
 }
 
 export const Project = ({data}: Props) => {
+  let fetcher = useFetcher()
   return (
-    <div className="flex justify-between border-b px-1 py-2 last-of-type:border-0">
+    <div className="flex flex-col justify-between border-b px-1 py-2 last-of-type:border-0">
       <p>{data.title}</p>
 
-      <div>
+      <div className="flex justify-end mt-2.5">
         <Link
           className="text-edit hover:text-edit/65 transition-colors duration-100"
           to={`/admin/project/${data.id}`}
@@ -18,9 +19,17 @@ export const Project = ({data}: Props) => {
           edit
         </Link>
         <span className="mx-1">•</span>
-        <button className="text-delete hover:text-delete/65 transition-colors duration-100">
-          delete
-        </button>
+
+        <fetcher.Form method="post">
+          <input type="hidden" name="projectId" value={data.id} />
+          <input type="hidden" name="intent" value="archive" />
+          <button
+            className="text-delete hover:text-delete/65 cursor-pointer transition-colors duration-100"
+            type="submit"
+          >
+            archive
+          </button>
+        </fetcher.Form>
       </div>
     </div>
   )
