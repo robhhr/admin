d9a6c03e2c8d60c93d9e6ffd9ba02c7dfe81aeaa
robhhr <robertohhr@pm.me>
2025-05-17 23:41:38 -0700

    sidebar position

--- app/components/modules/sidebar.tsx
+++ app/components/modules/sidebar.tsx
@@ -3,7 +3,7 @@ import {ToggleTheme} from './toggle-theme'
 
 export const Sidebar = () => {
   return (
-    <div className="bg-title-bar shadow-window absolute right-5 z-10 flex items-center justify-center px-2 py-1.5 top-5">
+    <div className="bg-title-bar shadow-window absolute top-5 right-5 z-10 flex items-center justify-center px-2 py-1.5 xl:top-16 xl:right-40">
       <ToggleTheme />
       <LogoutButton />
     </div>
