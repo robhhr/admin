0772cc8fa0846123ee1c045388d9d1262a189a07
robhhr <robertohhr@pm.me>
2025-05-18 02:29:03 -0700

    add form in create project

--- app/routes/admin.projects_.create.tsx
+++ app/routes/admin.projects_.create.tsx
@@ -1,22 +1,10 @@
 import {useEffect, useState} from 'react'
-import {
-  type ActionFunctionArgs,
-  Form,
-  redirect,
-  useActionData,
-  useNavigation,
-} from 'react-router'
+import {type ActionFunctionArgs, redirect, useActionData} from 'react-router'
 import {ControlsProjects} from '~/components/admin'
-import {Button} from '~/components/modules/button'
+import {ProjectForm} from '~/components/forms/projects'
 import {FeedbackDialog} from '~/components/ui/admin/dialog'
-import {InputText} from '~/components/ui/admin/input-text'
-import MetaControls from '~/components/ui/admin/meta'
-import {Select} from '~/components/ui/admin/select'
-import MarkdownEditor from '~/components/ui/admin/text-editor'
 import {isUserAuthenticated} from '~/models/auth.server'
 import {createProject} from '~/models/projects.server'
-
-// import type {CreateProjectFormProps} from '~/types/admin/forms'
 
 export const action = async ({request}: ActionFunctionArgs) => {
   const isAuth = await isUserAuthenticated(request)
@@ -48,16 +36,14 @@ export const action = async ({request}: ActionFunctionArgs) => {
 }
 
 const DashboardProjectsCreate = () => {
-  const [selected, setSelected] = useState('draft')
   const [error, setError] = useState<string | null>(null)
   const [success, setSuccess] = useState<string | null>(null)
-  const navigation = useNavigation()
   const actionData = useActionData<typeof action>()
   // console.log(actionData, ' actionData')
   // console.log(actionData && JSON.parse(actionData.meta))
 
   // console.log(navigation)
-  console.log(actionData, ' actionData')
+  // console.log(actionData, ' actionData')
 
   useEffect(() => {
     if (actionData?.error) {
@@ -73,53 +59,7 @@ const DashboardProjectsCreate = () => {
     <>
       <ControlsProjects />
 
-      <Form method="post" className="mt-4">
-        <input type="hidden" name="action" value="login" />
-
-        <div className="flex flex-col">
-          <label htmlFor="status" className="mb-1.5">
-            status
-          </label>
-
-          <Select
-            name="status"
-            options={[
-              {value: 'draft', label: 'draft'},
-              {value: 'publish', label: 'publish'},
-            ]}
-            value={selected}
-            onChange={e => {
-              setSelected(e.target.value)
-              handleChange()
-            }}
-          />
-        </div>
-
-        <div className="mt-2 flex flex-col">
-          <label htmlFor="title" className="mb-1.5">
-            title
-          </label>
-          <InputText name="title" onChange={handleChange} />
-        </div>
-
-        <div className="mt-2 mb-2.5 flex flex-col">
-          <MarkdownEditor setError={setError} />
-        </div>
-
-        <div className="mt-2 flex flex-col">
-          <MetaControls />
-        </div>
-
-        <Button
-          // disabled={navigation.state === 'submitting'}
-          disabled={navigation.state !== 'idle'}
-          className="mt-7"
-          intent="admin"
-          type="submit"
-        >
-          create
-        </Button>
-      </Form>
+      <ProjectForm handleChange={handleChange} setError={() => setError} />
 
       <FeedbackDialog
         actionData={error ? {error} : success ? {success} : undefined}
