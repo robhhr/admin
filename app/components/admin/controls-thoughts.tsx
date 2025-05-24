6e6f9a54adc73b77f8ef9959940ebda1a025c522
robhhr <robertohhr@pm.me>
2025-05-24 01:22:09 -0700

    control notes component

---
+++ app/components/admin/controls-thoughts.tsx
@@ -0,0 +1,27 @@
+import {useLocation} from 'react-router'
+import {Button} from '../modules/button'
+
+export const ControlsThoughts = () => {
+  const location = useLocation()
+
+  return (
+    <ul className="flex">
+      <li className="mr-2">
+        <Button
+          disabled={location.pathname === '/admin/thoughts'}
+          to="/admin/thoughts"
+        >
+          view all
+        </Button>
+      </li>
+      <li>
+        <Button
+          disabled={location.pathname === '/admin/thoughts/create'}
+          to="/admin/thoughts/create"
+        >
+          create
+        </Button>
+      </li>
+    </ul>
+  )
+}
--- app/components/admin/index.ts
+++ app/components/admin/index.ts
@@ -1,3 +1,4 @@
 export * from './controls-projects'
+export * from './controls-thoughts'
 export * from './nav'
 export * from './project-listing'
