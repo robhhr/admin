5e3da80fb1c30692347f7527b12d4fe65c2fbcc9
robhhr <robertohhr@pm.me>
2025-05-24 17:49:56 -0700

    enable tag nav

--- app/components/admin/controls-thoughts.tsx
+++ app/components/admin/controls-thoughts.tsx
@@ -5,21 +5,32 @@ export const ControlsThoughts = () => {
   const location = useLocation()
 
   return (
-    <ul className="flex">
-      <li className="mr-2">
+    <ul className="flex flex-col">
+      <div className="flex">
+        <li className="mr-2">
+          <Button
+            disabled={location.pathname === '/admin/thoughts'}
+            to="/admin/thoughts"
+          >
+            view all
+          </Button>
+        </li>
+        <li>
+          <Button
+            disabled={location.pathname === '/admin/thoughts/create'}
+            to="/admin/thoughts/create"
+          >
+            create
+          </Button>
+        </li>
+      </div>
+
+      <li className="mt-3">
         <Button
-          disabled={location.pathname === '/admin/thoughts'}
-          to="/admin/thoughts"
+          disabled={location.pathname === '/admin/thoughts/tags'}
+          to="/admin/thoughts/tags"
         >
-          view all
-        </Button>
-      </li>
-      <li>
-        <Button
-          disabled={location.pathname === '/admin/thoughts/create'}
-          to="/admin/thoughts/create"
-        >
-          create
+          tags
         </Button>
       </li>
     </ul>
